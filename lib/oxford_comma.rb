caa048abaaaa3d2f4378b25ee168d65e9a9ccb3f
wk645 <wk645@nyu.edu>
2017-06-12 22:59:01 -0400

    oxford lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+    if array.length < 3
+        answer = array.join
+        answer = array.join(" and ")
+    else
+        array.length >= 3
+        answer = array[0..-2].join(", ") + ", and " + array[-1]
+    end
 end
